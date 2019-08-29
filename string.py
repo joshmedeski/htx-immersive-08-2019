005f5208a0e3f340df9d7e1c90dd3e84a5fffddc
Josh Medeski <josh.medeski@gmail.com>
2019-08-29 10:39:39 -0500

    Be more descriptive with message

--- string.py
+++ string.py
@@ -1,5 +1,6 @@
 # Caluclate my age from the year I'm born
+# Be more descriptive
 
 year_born = 1993
 current_year = 2019
-print(current_year - year_born)
+print('I am ' + str( current_year - year_born ) + ' years old')
