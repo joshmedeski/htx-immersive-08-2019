6b26d68feef490e4206613e1c42dff696d925d8d
Josh Medeski <josh.medeski@gmail.com>
2019-08-29 10:47:14 -0500

    Get the current year dynamically

--- string.py
+++ string.py
@@ -1,6 +1,6 @@
-# Caluclate my age from the year I'm born
-# Be more descriptive
+import datetime
 
 year_born = int(input('What year were you born? '))
-current_year = int(input('What is the current year? '))
-print('I am ' + str( current_year - year_born ) + ' years old')
+now = datetime.datetime.now()
+current_year = now.year
+print('I am ' + str(current_year - year_born) + ' years old')
