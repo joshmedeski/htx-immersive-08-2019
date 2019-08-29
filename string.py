481b5422f06119a0a472e7221faac782a118ac85
Josh Medeski <josh.medeski@gmail.com>
2019-08-29 10:41:16 -0500

    Change year born into input

--- string.py
+++ string.py
@@ -1,6 +1,6 @@
 # Caluclate my age from the year I'm born
 # Be more descriptive
 
-year_born = 1993
+year_born = int(input('What year were you born? '))
 current_year = 2019
 print('I am ' + str( current_year - year_born ) + ' years old')
