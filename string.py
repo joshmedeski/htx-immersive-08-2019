c664628288bba5ff05589b6592ab094c460f0c9c
Josh Medeski <josh.medeski@gmail.com>
2019-08-29 10:49:22 -0500

    Josh's solution using string formatter

--- string.py
+++ string.py
@@ -3,4 +3,5 @@ import datetime
 year_born = int(input('What year were you born? '))
 now = datetime.datetime.now()
 current_year = now.year
-print('I am ' + str(current_year - year_born) + ' years old')
+years_old = current_year - year_born
+print(f'I am {years_old} years old')
