b3b6de3b3230777bf51f4193563017b357b8b8bf
Josh Medeski <josh.medeski@gmail.com>
2019-08-29 10:35:27 -0500

    Basic sample of caculating my age from the year I'm born

--- string.py
+++ string.py
@@ -1,24 +1,5 @@
-# 'String'
+# Caluclate my age from the year I'm born
 
-# print('String')
-# print('This ain\'t not a good thing')
-# print("This isn't a bad idea")
-# print("This is a\nnew line")
-
-print('hello '+'world')
-print('hello'+' world')
-print('hello' + ' ' +'world')
-# print('hello' + 'world')
-
-ranking_input = input('From 1-10, how happy are you?')
-ranking_int = int(ranking_input)
-
-ranking = int(input('From 1-10, how happy are you? '))
-# Times 100
-# print(ranking * 100)
-
-# half = 1/2
-# print(half)
-
-print(int(0.6))
-print(int(1.6))
+year_born = 1993
+current_year = 2019
+print(current_year - year_born)
