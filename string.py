c8714efcab698b59909a88a02b085f0a694ccd68
Josh Medeski <josh.medeski@gmail.com>
2019-08-29 10:42:52 -0500

    Change current year into input

--- string.py
+++ string.py
@@ -2,5 +2,5 @@
 # Be more descriptive
 
 year_born = int(input('What year were you born? '))
-current_year = 2019
+current_year = int(input('What is the current year? '))
 print('I am ' + str( current_year - year_born ) + ' years old')
