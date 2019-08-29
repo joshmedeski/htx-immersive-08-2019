b7aca0dcf632829223a91c8ef0af168bccd25b52
Josh Medeski <josh.medeski@gmail.com>
2019-08-29 10:30:08 -0500

    Adding basic lesson

---
+++ sample.py
@@ -0,0 +1,19 @@
+'String'
+# Input
+# input('What is your name? ')
+# print('Got it, I know your name now')
+
+# Variables
+minimum_age = 18
+maximum_age = 105
+name = 'Josh'
+# user_name = input('What is your name? ')
+user_name = 'Josh'
+print(user_name)
+print(f'My name is {user_name} {user_name}')
+user_name = 'Amy'
+print(user_name)
+
+user_name = 'Guy'
+user_name = 42
+print(user_name)
