555f73d88a8cb6feea17fc1304f388e2f5e68f05
Josh Medeski <josh.medeski@gmail.com>
2019-09-17 11:33:45 -0500

    Finished level 1

--- 04-week/1-tuesday/labs/js101-josh-solutions/exercises/level1.js
+++ 04-week/1-tuesday/labs/js101-josh-solutions/exercises/level1.js
@@ -1,34 +1,45 @@
-function lvl1exercise1 () {
-  // Declare a variable without instantiating it and return it!
-
+function lvl1exercise1() {
+  // Declare a variable without instantiating it
+  var thing;
+  // and return it!
+  return thing;
 }
 
-function lvl1exercise2 () {
-  // Declare and instantiate a number and return it
-
+function lvl1exercise2() {
+  // Declare and instantiate a number
+  var amount = 1;
+  // and return it
+  return amount;
 }
 
-function lvl1exercise3 () {
-  // Declare and instantiate a floating point number that is not a whole number and return it
-
+function lvl1exercise3() {
+  // Declare and instantiate a floating point number that is not a whole number
+  var partialAmount = 1.234;
+  // and return it
+  return partialAmount;
 }
 
-function lvl1exercise4 () {
-  // Declare and instantiate a string "Hello World!" and return it
-
+function lvl1exercise4() {
+  // Declare and instantiate a string "Hello World!"
+  var hello = "Hello World!";
+  // and return it
+  return hello;
 }
 
-function lvl1exercise5 () {
+function lvl1exercise5() {
   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
-
+  return ["Hello World!", 4];
 }
 
-function lvl1exercise6 () {
+function lvl1exercise6() {
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+  return {
+    key1: "Hello World!",
+    key2: 4
+  };
 }
 
-function lvl1exercise7 () {
+function lvl1exercise7() {
   // Declare and instantiate a boolean value 'false' and return it
-
+  return false;
 }
