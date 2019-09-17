f4091c6db01633df0a952f8ae993a86ade8d76f8
Josh Medeski <josh.medeski@gmail.com>
2019-09-17 11:26:42 -0500

    Add JS 101 lecture sample code

---
+++ 04-week/1-tuesday/lectures/sample-code.js
@@ -0,0 +1,20 @@
+// if this = that and whatever <= this:
+// if (this == that) && (whatever <= this) {
+
+7 == "7";
+
+var numbers = [0, 1, 2, 3, 4];
+
+numbers.forEach(number => {});
+
+function logThing(argument) {
+  console.log(argument);
+}
+
+function hello(name) {
+  var output = "Hello " + name;
+  logThing(output);
+  return output;
+}
+
+hello("josh");
