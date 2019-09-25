53f53a2c3994c0405667d27e851ee8111f9deba9
Josh Medeski <josh.medeski@gmail.com>
2019-09-24 20:55:17 -0500

    Add json-ajax lecture files

---
+++ 05-week/2-wednesday/lectures/get-ajax-data.js
@@ -0,0 +1,22 @@
+// Credit: https://en.wikipedia.org/wiki/Ajax_%28programming%29
+// This is the client-side script.
+
+// Initialize the HTTP request.
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "send-ajax-data.php");
+
+// Track the state changes of the request.
+xhr.onreadystatechange = function() {
+  var DONE = 4; // readyState 4 means the request is done.
+  var OK = 200; // status 200 is a successful return.
+  if (xhr.readyState === DONE) {
+    if (xhr.status === OK) {
+      console.log(xhr.responseText); // 'This is the output.'
+    } else {
+      console.log("Error: " + xhr.status); // An error occurred during the request.
+    }
+  }
+};
+
+// Send the request to send-ajax-data.php
+xhr.send(null);
