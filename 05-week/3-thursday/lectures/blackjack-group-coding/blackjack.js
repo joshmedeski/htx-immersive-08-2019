a8b19ecf9b0eb48fdafdbd357dc6ea362cc6fd0b
Josh Medeski <josh.medeski@gmail.com>
2019-09-26 10:26:37 -0500

    Create deck
    I decided to make all face cards points' 10
    I also decided to create a separate nume key for each card

--- 05-week/3-thursday/lectures/blackjack-group-coding/blackjack.js
+++ 05-week/3-thursday/lectures/blackjack-group-coding/blackjack.js
@@ -1,3 +1,34 @@
+var deck = generateDeck();
+
+function generateDeck() {
+  let tempDeck = [];
+  for (let i = 1; i <= 13; i++) {
+    ["clubs", "hearts", "spades", "diamonds"].forEach(suit => {
+      tempDeck.push({
+        point: i > 10 ? 10 : i,
+        name: cardName(i),
+        suit: suit
+      });
+    });
+  }
+  return tempDeck;
+}
+
+function cardName(number) {
+  switch (number) {
+    case 1:
+      return "ace";
+    case 11:
+      return "jack";
+    case 12:
+      return "queen";
+    case 13:
+      return "king";
+    default:
+      return number;
+  }
+}
+
 function card() {
   let card = document.createElement("img");
   card.setAttribute("src", "card-images/2_of_clubs.png");
