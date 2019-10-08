6141586ec227124da56ee21f1a351dce5feec1b1
Josh Medeski <josh.medeski@gmail.com>
2019-10-08 16:37:36 -0500

    Add basic Pug exercise

---
+++ 07-week/2-wednesday/lectures/server-side-pages/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Pug", message: "Welcome to Pug!" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Us" });
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));
