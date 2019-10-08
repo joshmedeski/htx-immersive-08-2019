01ec4ab5c87a7c67d9d4acbd8d0338e709825a92
Josh Medeski <josh.medeski@gmail.com>
2019-10-08 16:01:25 -0500

    "fix" node modules issue and update app

--- 07-week/1-tuesday/lectures/express-hello-world/app.js
+++ 07-week/1-tuesday/lectures/express-hello-world/app.js
@@ -1,9 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Hello People");
+});
+
+// /movies?sort=asc&by=title
+app.get("/movies", (req, res) => {
+  console.log(req.query.sort);
+  console.log(req.query.by);
+  res.json([
+    { title: "Lord of the Rings", year: 2001 },
+    { title: "Star Wars", year: 1977 }
+  ]);
+});
+
+app.get("/genres", (req, res) => {
+  res.json(["Action", "Horror", "Thriller", "Comedy"]);
+});
+
+app.get("/genres/:genre", (req, res) => {
+  res.send(`${req.param.genre} movies`);
+});
+
+app.post("/movies", (req, res) => {
+  console.log(req.body);
+  res.send(`${req.body.title} added successfully`);
 });
 
 app.listen(PORT, () => {
