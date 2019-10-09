c4f3d2bbb657e8db158b21dbc5cefdc6bad066be
Josh Medeski <josh.medeski@gmail.com>
2019-10-09 10:41:11 -0500

    Add movies example with loops and params

--- 07-week/2-wednesday/lectures/templating-language/app.js
+++ 07-week/2-wednesday/lectures/templating-language/app.js
@@ -9,6 +9,31 @@ app.get("/", function(req, res) {
   res.render("index", { title: "Hey", message: "Hello there!" });
 });
 
+const movies = [
+  { title: "Lord of the Rings", slug: "lotr" },
+  { title: "Star Wars", slug: "starwars" },
+  { title: "Pulp Fiction", slug: "pulpfiction" },
+  { title: "The Ring", slug: "thering" },
+  { title: "Coco", slug: "coco" },
+  { title: "Up", slug: "up" },
+  { title: "Finding Nemo", slug: "findingnemo" }
+];
+
+app.get("/movies", function(req, res) {
+  res.render("movies", { movies: movies });
+});
+
+app.get("/movies/:movie", function(req, res) {
+  let filteredMovies = movies.filter(movie => {
+    return movie.slug === req.params.movie;
+  });
+  console.log(filteredMovies);
+  if (filteredMovies.length < 1) {
+    res.send("Movie not found");
+  }
+  res.render("movie", { movie: filteredMovies[0] });
+});
+
 app.get("/about/us", function(req, res) {
   res.render("about", { title: "Hey", message: "Hello there!" });
 });
