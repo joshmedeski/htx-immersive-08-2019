eeb7b8954e28a67c204f5acac42b0e9ca99e1bb7
Josh Medeski <josh.medeski@gmail.com>
2019-10-09 10:21:21 -0500

    Add static resources

--- 07-week/2-wednesday/lectures/templating-language/app.js
+++ 07-week/2-wednesday/lectures/templating-language/app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
+app.use(express.static("public"));
 app.set("view engine", "pug");
 
 app.get("/", function(req, res) {
   res.render("index", { title: "Hey", message: "Hello there!" });
+});
+
+app.get("/about/us", function(req, res) {
+  res.render("about", { title: "Hey", message: "Hello there!" });
 });
 
 app.listen(port, () => {
