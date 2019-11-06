ec6ba1926ab2ce91ccb7a8fe3a50609d689f4800
Josh Medeski <josh.medeski@gmail.com>
2019-11-06 10:06:00 -0600

    Create working clock with state

--- 11-week/2-wednesday/lectures/clock-inclass/src/components/Clock.js
+++ 11-week/2-wednesday/lectures/clock-inclass/src/components/Clock.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 
 export default class Clock extends Component {
+  constructor() {
+    super();
+    this.state = { time: new Date(), people: 21 };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ time: new Date() });
+    }, 1000);
+  }
+
   render() {
-    const now = new Date();
-    return <h1>{now.toLocaleTimeString()}</h1>;
+    return (
+      <>
+        <h1>{this.state.time.toLocaleTimeString()}</h1>
+        <h2>Number of People: {this.state.people}</h2>
+      </>
+    );
   }
 }
