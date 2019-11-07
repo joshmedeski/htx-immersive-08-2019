652ed19563a779fc5b9dfa59aa299b1640e6c4b8
Josh Medeski <josh.medeski@gmail.com>
2019-11-07 11:01:37 -0600

    Passing arguments into callback functions

--- 11-week/3-thursday/lectures/event-handling-inclass/src/App.js
+++ 11-week/3-thursday/lectures/event-handling-inclass/src/App.js
@@ -11,9 +11,9 @@ export default class App extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
+  handleClick(id, name) {
     this.setState({ isActivate: true });
-    alert("activate lasers");
+    alert(`activate laser ${id}, congrats ${name}`);
   }
 
   render() {
@@ -22,9 +22,15 @@ export default class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <button onClick={this.handleClick}>Activate Lasers</button>
-          {/* <button onClick={e => this.handleClick(e)}>
-            Activate Lasers Another Way
-          </button> */}
+          <button onClick={e => this.handleClick("1", "josh", e)}>
+            Activate Laser 1
+          </button>
+          <button onClick={this.handleClick.bind(this, "2", "josh")}>
+            Activate Lasers 2
+          </button>
+          <button onClick={this.handleClick.bind(this, "4", "scott")}>
+            Activate Lasers 4
+          </button>
           <p>
             Are the lasers activated? {this.state.isActivate ? "true" : "false"}
           </p>
