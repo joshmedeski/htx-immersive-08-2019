c536fc8f338316a7ffbb4bda3f9bf2a7b46d9438
Josh Medeski <josh.medeski@gmail.com>
2019-11-07 11:14:23 -0600

    Create simple login/logout conditionals

--- 11-week/3-thursday/lectures/conditional-rendering/src/App.js
+++ 11-week/3-thursday/lectures/conditional-rendering/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+var isLoggedIn = true;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <UserStatusButton name="Josh" />
+        {/* <p>{isLoggedIn ? "Logout" : "Login"}</p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +22,22 @@ function App() {
       </header>
     </div>
   );
+}
+
+function UserStatusButton(props) {
+  if (isLoggedIn) {
+    return <Logout name={props.name} />;
+  } else {
+    return <Login name={props.name} />;
+  }
+}
+
+function Login(props) {
+  return <button>Log {props.name} In</button>;
+}
+
+function Logout(props) {
+  return <button>Log {props.name} Out</button>;
 }
 
 export default App;
