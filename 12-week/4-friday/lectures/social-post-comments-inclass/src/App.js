59da6e2b4521d6ef6891bb3fef94da8d99c1ed48
Josh Medeski <josh.medeski@gmail.com>
2019-11-15 11:01:58 -0600

    Create basic HTML structure

--- 12-week/4-friday/lectures/social-post-comments-inclass/src/App.js
+++ 12-week/4-friday/lectures/social-post-comments-inclass/src/App.js
@@ -1,5 +1,52 @@
 import React from "react";
 
 export default function App() {
-  return <h1>Social Post Comments Exercise (In Class)</h1>;
+  return (
+    <div className="container">
+      <h1>Posts</h1>
+      <h2>Create New Post</h2>
+      <form className="post-form">
+        <label>Name</label>
+        <input
+          name="post-form-name"
+          type="text"
+          placeholder="Enter your name"
+        />
+        <textarea name="post-form-content" cols="30" rows="10"></textarea>
+        <button>Submit</button>
+      </form>
+      {/* loop through post */}
+      <div className="post">
+        <p className="post-name">
+          <strong>Josh</strong>
+        </p>
+        <p className="post-content">Hello world, this is my first post.</p>
+        <div className="post-comments">
+          <h3>Comments</h3>
+          {/* loop through post comment */}
+          <div className="post-comment">
+            <p className="post-comment-name">
+              <strong>Scott</strong>
+            </p>
+            <p className="post-comment-content">First</p>
+          </div>
+          <form className="post-comments-form">
+            <h3>Create New Comment</h3>
+            <label>Name</label>
+            <input
+              type="text"
+              name="post-comment-form-name"
+              placeholder="Enter your name"
+            />
+            <input
+              type="text"
+              name="post-comment-form-content"
+              placeholder="Enter your comment"
+            />
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
