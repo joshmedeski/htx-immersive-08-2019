291089f732885c61d1aa6ebf7991d879ae1f8f5e
Josh Medeski <josh.medeski@gmail.com>
2019-11-19 11:05:24 -0600

    Get simple react redux store working

--- 13-week/1-tuesday/labs/redux-with-react-inclass-josh/src/App.js
+++ 13-week/1-tuesday/labs/redux-with-react-inclass-josh/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { store as appStore } from "./redux/store";
 
 function App() {
   return (
-    <>
+    <Provider store={appStore}>
       <h1>Redux with React</h1>
       <p>Simple counter exercise</p>
-    </>
+    </Provider>
   );
 }
 
